82bb8820be9456622621bc35cf6d546b0dd2f4a0
Rohankc073 <rohankc073@gmail.com>
2024-12-08 12:22:49 +0545

    Card_View

--- lib/view/dynamic_card_view.dart
+++ lib/view/dynamic_card_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:project_flutter/common/mysnackbar.dart';
+import 'package:project_flutter/view/card_view.dart';
+
 class DynamicCardView extends StatelessWidget {
   const DynamicCardView({super.key});
 
@@ -14,41 +17,19 @@ class DynamicCardView extends StatelessWidget {
         child: Column(
           children: [
             for (int i = 1; i < 21; i++) ...{
-              MyCard(text: "Card $i"),
+              GestureDetector(
+                onTap: () {
+                  showMySnackBar(
+                    context,
+                    'Card $i Clicked',
+                  );
+                },
+                child: MyCard(text: 'Card $i '),
+              )
             }
           ],
         ),
       )),
-    );
-  }
-}
-
-class MyCard extends StatelessWidget {
-  const MyCard({
-    super.key,
-    required this.text,
-    this.color,
-  });
-
-  final String text;
-  final Color? color;
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 200,
-      width: double.infinity,
-      child: Card(
-        color: color ?? Colors.amber,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Center(
-            child: Text(
-          text,
-          style: const TextStyle(fontSize: 20),
-        )),
-      ),
     );
   }
 }
