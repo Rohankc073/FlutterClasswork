e52ea225a720474a58b2ca2a6c6c4ead3b4e5aaa
Rohankc073 <rohankc073@gmail.com>
2024-12-15 10:51:30 +0545

    5 Screens

--- lib/view/bottom_navigation_view.dart
+++ lib/view/bottom_navigation_view.dart
@@ -1,13 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:project_flutter/view/bottom_screen/about_view.dart';
+import 'package:project_flutter/view/bottom_screen/cart_view.dart';
+import 'package:project_flutter/view/bottom_screen/home_view.dart';
+import 'package:project_flutter/view/bottom_screen/profile_view.dart';
 
-class BottomNavigationView extends StatelessWidget {
+class BottomNavigationView extends StatefulWidget {
   const BottomNavigationView({super.key});
+
+  @override
+  State<BottomNavigationView> createState() => _BottomNavigationViewState();
+}
+
+class _BottomNavigationViewState extends State<BottomNavigationView> {
+  int _selectedIndex = 0;
+
+  List<Widget> lstBottomScreen = [
+    const AboutView(),
+    const CartView(),
+    const HomeView(),
+    const ProfileView(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Bottom Navigation"),
+        centerTitle: true,
+      ),
+      body: lstBottomScreen[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_bag), label: "Cart"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile")
+        ],
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
       ),
     );
   }
